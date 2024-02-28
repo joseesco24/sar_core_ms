9b6483791103c103f63a69666ff92b803a3f0681
joseesco24 <josed.escobar@outlook.com>
2024-02-28 12:11:48 -0500

    feat: improves on connection manager switch braking

--- src/database/session_managers/mysql_sar_manager.py
+++ src/database/session_managers/mysql_sar_manager.py
@@ -8,6 +8,10 @@ import gc
 # ** info: typing imports
 from typing import Self
 
+# ** info: fastapi imports
+from fastapi import HTTPException
+from fastapi import status
+
 # **info: sqlalchemy imports
 from sqlalchemy import text
 from sqlalchemy import URL
@@ -46,28 +50,52 @@ class MySQLSarManager:
         self._post_init()
 
     def obtain_session(self: Self) -> Session:
+        logging.info("obtaining connection")
+
+        self._check_session_health()
+
+        logging.info("connection obtained")
+
         instanes_ids: str = r",".join(str(s) for s in MySQLSarManager.instances)
         instances_count: int = MySQLSarManager.instances.__len__()
-        logging.info("obtaining connection")
         logging.debug(f"instances count: {instances_count}")
         logging.debug(f"instances ids: {instanes_ids}")
-        self._check_session_health()
+
         return self._session
 
     def _check_session_health(self: Self) -> None:
-        is_connection_healthy: bool
+        if self._test_qeury() is True:
+            logging.info(f"connection {self._connection_id} is healthy")
+            return
+
+        if self._connection_id is None:
+            logging.critical(r"connection not established trying to stablish a new one")
+        else:
+            logging.critical(f"connection {self._connection_id} is unhealthy")
+
+        self._restart_session()
+
+        if self._test_qeury() is True:
+            logging.info(f"connection {self._connection_id} is healthy")
+            return
+
+        logging.critical("connection is still unhealthy after restart")
+        logging.critical("shutting down connection")
+        self._end_session_and_engine()
+        logging.critical("connection shuted down")
+        logging.critical("a new attempt to restart the connection is going to be executed on the next request")
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def _test_qeury(self: Self) -> bool:
+        if self._connection_id is None:
+            return False
+
         try:
             self._session.exec(text(r"select 1"))
-            is_connection_healthy = True
-        except Exception as e:
-            print(e)
-            is_connection_healthy = False
-        if is_connection_healthy is False:
-            logging.critical(f"connection {self._connection_id} is unhealthy")
-            self._restart_session()
-            logging.info(f"new connection {self._connection_id} is healthy")
-        else:
-            logging.info(f"connection {self._connection_id} is healthy")
+            return True
+
+        except Exception:
+            return False
 
     def _restart_session(self: Self) -> None:
         logging.warning("restarting connection")
@@ -76,29 +104,46 @@ class MySQLSarManager:
         logging.warning("connection restarted")
 
     def _end_session_and_engine(self: Self) -> None:
+        if self._connection_id is None:
+            return
+
         logging.warning(f"ending connection {self._connection_id}")
+
         if self._session is not None:
             self._session.close()
             del self._session
             gc.collect()
             self._session = None
+
         if self._engine is not None:
             self._engine.dispose()
             del self._engine
             gc.collect()
             self._engine = None
-        logging.warning(f"connection {self._connection_id} ended")
-        MySQLSarManager.instances.remove(self._connection_id)
+
+        if self._connection_id is not None:
+            MySQLSarManager.instances.remove(self._connection_id)
+            self._connection_id = None
+
+        logging.warning("connection ended")
 
     def _start_session_and_engine(self: Self) -> None:
-        self._connection_id = uuid_provider.get_str_uuid()
-        logging.warning(f"starting new connection {self._connection_id}")
+        if self._connection_id is not None:
+            return
+
+        logging.warning("starting new connection")
+
+        if self._connection_id is None:
+            self._connection_id = uuid_provider.get_str_uuid()
+            MySQLSarManager.instances.add(self._connection_id)
+
         if self._engine is None:
             self._engine = create_engine(self._url, echo=configs.database_logs)
+
         if self._session is None:
             self._session = Session(bind=self._engine)
+
         logging.warning(f"new connection {self._connection_id} started")
 
     def _post_init(self: Self) -> None:
-        logging.info(f"connection started with id {self._connection_id}")
         MySQLSarManager.instances.add(self._connection_id)
