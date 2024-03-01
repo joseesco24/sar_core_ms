f6e832f168daf00a43a068dae287c86c210a727b
joseesco24 <josed.escobar@outlook.com>
2024-03-01 17:03:07 -0500

    refactor: move from layered to hexagonal architecture

--- src/sar_ms_py.py
+++ src/sar_ms_py.py
@@ -66,7 +66,7 @@ metadata: Dict[str, Any] = {
     "description": "This repository corresponds to the a small python microservice that is gint to be used used in the sar system.",
     "summary": "Service incharge of managing wastes, collect request, and system parameters.",
     "title": "Sar Python Microservice",
-    "version": "v1.6.1",
+    "version": "v2.0.0",
 }
 
 sar_ms_py: FastAPI
