ace209654ba312fc35f58a0f525a5c1447e649f3
joseesco24 <josed.escobar@outlook.com>
2024-03-01 21:05:07 -0500

    feat: version increase due to last changes

--- src/sar_ms_py.py
+++ src/sar_ms_py.py
@@ -66,7 +66,7 @@ metadata: Dict[str, Any] = {
     "description": "This repository corresponds to the a small python microservice that is gint to be used used in the sar system.",
     "summary": "Service incharge of managing wastes, collect request, and system parameters.",
     "title": "Sar Python Microservice",
-    "version": "v2.0.2",
+    "version": "v2.0.3",
 }
 
 sar_ms_py: FastAPI
