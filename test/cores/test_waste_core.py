b9c9d2e81385fd1c2a3b058c3431361897f9e639
joseesco24 <josed.escobar@outlook.com>
2024-03-07 20:04:36 -0500

    test: extend waste core tests

--- test/cores/test_waste_core.py
+++ test/cores/test_waste_core.py
@@ -14,6 +14,7 @@ sys.path.append(join(path.dirname(path.realpath(__file__)), "..", "."))
 
 # ** info: dtos imports
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteClasificationResponseDto  # type: ignore
+from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteFullDataResponseListDto  # type: ignore
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteFullDataResponseDto  # type: ignore
 
 # ** info: core imports
@@ -21,9 +22,12 @@ from src.modules.waste.cores.business.waste_core import WasteCore  # type: ignor
 
 # ** info: fixtures imports
 from test_waste_core_fixtures import update_waste_casification_request_fixture_1  # type: ignore
+from test_waste_core_fixtures import waste_filter_by_status_request_fixture_1  # type: ignore
+from test_waste_core_fixtures import waste_full_data_list_response_fixture_1  # type: ignore
 from test_waste_core_fixtures import waste_clasification_response_fixture_1  # type: ignore
 from test_waste_core_fixtures import parameter_search_request_fixture_1  # type: ignore
 from test_waste_core_fixtures import waste_full_data_response_fixture_1  # type: ignore
+from test_waste_core_fixtures import wastes_list  # type: ignore
 from test_waste_core_fixtures import waste_1  # type: ignore
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -33,6 +37,7 @@ from test_waste_core_fixtures import waste_1  # type: ignore
 waste_core: WasteCore = WasteCore()
 waste_core._parameter_core.cpm_pc_get_set_of_parameter_ids_by_domain = AsyncMock(return_value=set([1]))  # type: ignore
 waste_core._waste_provider.update_waste_internal_classification_info = MagicMock(return_value=waste_1)  # type: ignore
+waste_core._waste_provider.list_wastes_by_process_status = MagicMock(return_value=wastes_list)  # type: ignore
 waste_core._brms_service.obtain_waste_clasification = MagicMock(return_value=1)  # type: ignore
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -42,21 +47,29 @@ waste_core._brms_service.obtain_waste_clasification = MagicMock(return_value=1) 
 
 @mark.asyncio
 async def test_driver_obtain_waste_classify_1() -> None:
-    waste_clasification_response: WasteClasificationResponseDto = await waste_core.driver_obtain_waste_classify(parameter_search_request=parameter_search_request_fixture_1)
+    obtain_waste_classify_response: WasteClasificationResponseDto = await waste_core.driver_obtain_waste_classify(parameter_search_request=parameter_search_request_fixture_1)
     waste_core._brms_service.obtain_waste_clasification.assert_called_with(
         state_waste=parameter_search_request_fixture_1.stateWaste,
         isotopes_number=parameter_search_request_fixture_1.isotopesNumber,
         weight_in_kg=parameter_search_request_fixture_1.weightInKg,
     )
-    assert waste_clasification_response == waste_clasification_response_fixture_1
+    assert obtain_waste_classify_response == waste_clasification_response_fixture_1
 
 
 @mark.asyncio
 async def test_driver_update_waste_classify_classify_1() -> None:
-    waste_clasification_response: WasteFullDataResponseDto = await waste_core.driver_update_waste_classify(waste_classify_request=update_waste_casification_request_fixture_1)
+    update_waste_classify_response: WasteFullDataResponseDto = await waste_core.driver_update_waste_classify(waste_classify_request=update_waste_casification_request_fixture_1)
     waste_core._brms_service.obtain_waste_clasification.assert_called_with(
         state_waste=parameter_search_request_fixture_1.stateWaste,
         isotopes_number=parameter_search_request_fixture_1.isotopesNumber,
         weight_in_kg=parameter_search_request_fixture_1.weightInKg,
     )
-    assert waste_clasification_response == waste_full_data_response_fixture_1
+    assert update_waste_classify_response == waste_full_data_response_fixture_1
+
+
+@mark.asyncio
+async def test_driver_search_waste_by_status_1() -> None:
+    waste_core._parameter_core.cpm_pc_get_set_of_parameter_ids_by_domain = AsyncMock(return_value=set([9]))  # type: ignore
+    filtered_wastes_response: WasteFullDataResponseListDto = await waste_core.driver_search_waste_by_status(filter_waste_by_status_request=waste_filter_by_status_request_fixture_1)
+    waste_core._waste_provider.list_wastes_by_process_status.assert_called_with(process_status=9)
+    assert filtered_wastes_response == waste_full_data_list_response_fixture_1
--- test/cores/test_waste_core_fixtures.py
+++ test/cores/test_waste_core_fixtures.py
@@ -5,12 +5,17 @@ from os.path import join
 from os import path
 import sys
 
+# ** info: typing imports
+from typing import List
+
 # **info: appending src path to the system paths for absolute imports from src path
 sys.path.append(join(path.dirname(path.realpath(__file__)), "..", "..", "."))
 
 # ** info: dtos imports
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteClasificationResponseDto  # type: ignore
+from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteFilterByStatusRequestDto  # type: ignore
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteClasificationRequestDto  # type: ignore
+from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteFullDataResponseListDto  # type: ignore
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteFullDataResponseDto  # type: ignore
 from src.modules.waste.ports.rest_routers_dtos.waste_dtos import WasteClassifyRequestDto  # type: ignore
 
@@ -30,6 +35,8 @@ datetime_provider: DatetimeProvider = DatetimeProvider()
 # ** info: waste entite fixtures declaration
 # ---------------------------------------------------------------------------------------------------------------------
 
+wastes_list: List[Waste] = list()
+
 waste_1: Waste = Waste(
     uuid="08893dbf-ebd1-4717-988b-fd15ddff12c9",
     request_uuid="9484e5da-0987-45bd-a359-44702769aaad",
@@ -47,9 +54,31 @@ waste_1: Waste = Waste(
     update=datetime_provider.get_current_time(),
 )
 
+waste_2: Waste = Waste(
+    uuid="08893dbf-ebd1-4717-988b-fd15ddff12c9",
+    request_uuid="9484e5da-0987-45bd-a359-44702769aaad",
+    type=1,
+    packaging=1,
+    weight_in_kg=float(20),  # type: ignore
+    volume_in_l=float(10),  # type: ignore
+    description="",
+    note="",
+    process_status=9,
+    store=1,
+    state_waste=1,
+    isotopes_number=float(4.0),  # type: ignore
+    create=datetime_provider.get_current_time(),
+    update=datetime_provider.get_current_time(),
+)
+
+wastes_list.append(waste_1)
+wastes_list.append(waste_2)
+
 # ---------------------------------------------------------------------------------------------------------------------
 # ** info: waste full data response dtos declaration
 # ---------------------------------------------------------------------------------------------------------------------
+
+waste_full_data_response_fixture_list_1: List[WasteFullDataResponseDto] = list()
 
 waste_full_data_response_fixture_1: WasteFullDataResponseDto = WasteFullDataResponseDto(
     id="08893dbf-ebd1-4717-988b-fd15ddff12c9",
@@ -67,6 +96,28 @@ waste_full_data_response_fixture_1: WasteFullDataResponseDto = WasteFullDataResp
     create=datetime_provider.prettify_date_time_obj(date_time_obj=waste_1.create),
     update=datetime_provider.prettify_date_time_obj(date_time_obj=waste_1.update),
 )
+
+waste_full_data_response_fixture_2: WasteFullDataResponseDto = WasteFullDataResponseDto(
+    id="08893dbf-ebd1-4717-988b-fd15ddff12c9",
+    requestId="9484e5da-0987-45bd-a359-44702769aaad",
+    type=1,
+    packaging=1,
+    processStatus=9,
+    weightInKg=20.0,
+    volumeInL=10.0,
+    isotopesNumber=4.0,
+    stateWaste=1,
+    storeType=1,
+    description="",
+    note="",
+    create=datetime_provider.prettify_date_time_obj(date_time_obj=waste_2.create),
+    update=datetime_provider.prettify_date_time_obj(date_time_obj=waste_2.update),
+)
+
+waste_full_data_response_fixture_list_1.append(waste_full_data_response_fixture_1)
+waste_full_data_response_fixture_list_1.append(waste_full_data_response_fixture_2)
+
+waste_full_data_list_response_fixture_1: WasteFullDataResponseListDto = WasteFullDataResponseListDto(values=waste_full_data_response_fixture_list_1)
 
 # ---------------------------------------------------------------------------------------------------------------------
 # ** info: parameter search request dtos fixtures declaration
@@ -95,4 +146,12 @@ update_waste_casification_request_fixture_1: WasteClassifyRequestDto = WasteClas
     isotopesNumber=float(1.0),
     stateWaste=1,
     storeId=1,
+)
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ** info: waste filter by status request dto fixtures declaration
+# ---------------------------------------------------------------------------------------------------------------------
+
+waste_filter_by_status_request_fixture_1: WasteFilterByStatusRequestDto = WasteFilterByStatusRequestDto(
+    processStatus=9,
 )
