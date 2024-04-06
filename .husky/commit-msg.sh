5b37ec4972d8c4befb714673ebaa4c660c56b79a
joseesco24 <josed.escobar@outlook.com>
2024-04-06 00:24:30 -0500

    style: improvements in prettier usage with ignore

--- .husky/commit-msg.sh
+++ .husky/commit-msg.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 print_title() {
-    TIT='\033[1;33m'
-    NCL='\033[0m'
-    title=$1
+	TIT='\033[1;33m'
+	NCL='\033[0m'
+	title=$1
 
-    set -e
+	set -e
 
-    echo -e
-    echo -e "${TIT}${title}${NCL}"
-    echo -e
+	echo -e
+	echo -e "${TIT}${title}${NCL}"
+	echo -e
 }
 
 print_title "Linting Commit"
--- .husky/pre-commit.sh
+++ .husky/pre-commit.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 print_title() {
-    TIT='\033[1;33m'
-    NCL='\033[0m'
-    title=$1
+	TIT='\033[1;33m'
+	NCL='\033[0m'
+	title=$1
 
-    set -e
+	set -e
 
-    echo -e
-    echo -e "${TIT}${title}${NCL}"
-    echo -e
+	echo -e
+	echo -e "${TIT}${title}${NCL}"
+	echo -e
 }
 
 # ** info: fetching changed files list
@@ -19,15 +19,15 @@ cero_element=${staged_files[0]}
 
 # ** info: printing changed files list
 if ([ $changed_files_count != 0 ] && [ "$cero_element" != "" ]); then
-    print_title "Changed Files Count On This Commit"
-    printf 'files in stage area: %i' $changed_files_count
-    echo -e
-    print_title "Changed Files List On This Commit"
-    for staged_file in "${staged_files[@]}"; do
-        echo $staged_file
-    done
+	print_title "Changed Files Count On This Commit"
+	printf 'files in stage area: %i' $changed_files_count
+	echo -e
+	print_title "Changed Files List On This Commit"
+	for staged_file in "${staged_files[@]}"; do
+		echo $staged_file
+	done
 else
-    print_title "There Are Not Changed Files On This Commit"
+	print_title "There Are Not Changed Files On This Commit"
 fi
 
 # ** info: executing tests
@@ -46,24 +46,24 @@ black ./test/unit_test
 printf "\n"
 prettier ./test/load_test/k6/**/*.js --write
 printf "\n"
-prettier ./**/*.{yaml,json,md,graphql} --write
+prettier "./**/*.{yaml,json,md,graphql,sh,env}|Dockerfile" --write
 
 # ** info: exporting dependencies if needed
 if [[ " ${staged_files[@]} " =~ " poetry.lock " ]]; then
-    print_title "Exporting Dependencies"
-    poetry export --without-hashes --only dev --format=requirements.txt >./req/dev.txt
-    poetry export --without-hashes --format=requirements.txt >./req/app.txt
-    git add ./req/app.txt
-    git add ./req/dev.txt
+	print_title "Exporting Dependencies"
+	poetry export --without-hashes --only dev --format=requirements.txt > ./req/dev.txt
+	poetry export --without-hashes --format=requirements.txt > ./req/app.txt
+	git add ./req/app.txt
+	git add ./req/dev.txt
 fi
 
 # ** info: updating staged files
 if ([ $changed_files_count != 0 ] && [ "$cero_element" != "" ]); then
-    for staged_file in "${staged_files[@]}"; do
-        if test -f "$staged_file"; then
-            git add $staged_file
-        fi
-    done
+	for staged_file in "${staged_files[@]}"; do
+		if test -f "$staged_file"; then
+			git add $staged_file
+		fi
+	done
 fi
 
 # ** info: linting files
